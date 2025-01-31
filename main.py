11d05efbdcd39b4dbcbd47663b45ea588116fe78
fabrizio-hmg <f.cau@handelsblattgroup.com>
2025-01-31 14:31:41 +0100

    Updated API logic

--- main.py
+++ main.py
@@ -10,46 +10,53 @@ headers = {
 
 def main():
     
-    analyst = Insight()
     st.title('HB BK-Umfrage (2024)')
     st.markdown('''
-        1. Upload the provided dataset in CSV format.  
-        2. Filter the data based on your criteria.  
-        3. Select only the most relevant column(s) for your question.  
-        4. Ask ChatGPT your question.  
+        1. Enter your OpenAI API Key.
+        2. Upload the provided dataset in CSV format.  
+        3. Filter the data based on your criteria.  
+        4. Select only the most relevant column(s) for your question.  
+        5. Ask ChatGPT your question.  
     ''')
     st.divider()
 
+    openai_api_key = st.text_input('OpenAI API Key', type='password')
+    if not openai_api_key:
+        st.warning('Please add your OpenAI API Key to continue')
+        st.stop()
+
+    analyst = Insight()
     uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.write(df.head())
-
-        columns = df.columns.to_list()
-        selected_columns = st.selectbox('Select column to filter by', columns)
-        unique_values = df[selected_columns].unique()
-        selected_value = st.selectbox('Select value', unique_values)
-
-        filtered_df = df[df[selected_columns] == selected_value]
-        st.write(filtered_df)
-
-        relevenat_columns = st.multiselect('Select only relevant columns', filtered_df.columns[2:])
-        relevant_df = filtered_df[relevenat_columns]
+    try:
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            st.write(df.head())
     
-        if relevenat_columns:
-            st.write(relevant_df)
-
-        prompt = st.text_area('Ask a question')
-
-        if prompt:
-            insight = analyst.generate_insight(relevant_df, prompt)
+            columns = df.columns.to_list()
+            selected_columns = st.selectbox('Select column to filter by', columns)
+            unique_values = df[selected_columns].unique()
+            selected_value = st.selectbox('Select value', unique_values)
+    
+            filtered_df = df[df[selected_columns] == selected_value]
+            st.write(filtered_df)
+    
+            relevenat_columns = st.multiselect('Select only relevant columns', filtered_df.columns[2:])
+            relevant_df = filtered_df[relevenat_columns]
         
-        if st.button('Generate Insights'):
-            st.write(insight.insight)
+            if relevenat_columns:
+                st.write(relevant_df)
+    
+            prompt = st.text_area('Ask a question')
+    
+            if prompt:
+                insight = analyst.generate_insight(relevant_df, prompt)
+            
+            if st.button('Generate Insights'):
+                st.write(insight.insight)
+    except:
+        st.error('Invalid credentials. Please try again.')
 
-    else:
-        st.write('No file uploaded')
 
 if __name__ == '__main__':
     main()
